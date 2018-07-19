8ef339fcf54d9f26ccbbe329d20a9b888c8e8bec
chetnuy <chetnuy@gmail.com>
2018-07-19 04:18:46 +0300

    many new function

--- posinstall.ps1
+++ posinstall.ps1
@@ -87,12 +87,13 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\FileHistory" /v "Disabled" /t 
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "DeviceHistoryEnabled" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v "LetAppsAccessCallHistory" /t REG_DWORD /d 0 /f
 
-#Отключаем people
-red add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v "PeopleBand" /t REG_DWORD /d 0 /f
+echo oтключаем people
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v "PeopleBand" /t REG_DWORD /d 0 /f
+
 
 #onedrive delete
 taskkill /f /im OneDrive.exe
-start %SystemRoot%\System32\OneDriveSetup.exe /uninstall
+cmd /c %SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
 
 #создаем папку с настройками
 cmd /c "mkdir c:\Users\root\Desktop\settings.{ED7BA470-8E54-465E-825C-99712043E01C}"
